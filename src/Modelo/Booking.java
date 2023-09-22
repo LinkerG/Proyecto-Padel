6095221f6e71c06bd5af9ef17eecf201edab53fa
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2023-09-22 09:46:10 +0200

    Clase Booking y clase Court

--- src/Modelo/Booking.java
+++ src/Modelo/Booking.java
@@ -1,5 +1,52 @@
 package Modelo;
 
+import java.util.Calendar;
+
 public class Booking {
+    // Attributes
+    private int courtId;
+    private String userDNI;
+    private Calendar day;
+    private int hour;
     
+    // Constructor
+    public Booking(int courtId, String userDNI, Calendar day, int hour) {
+        this.courtId = courtId;
+        this.userDNI = userDNI;
+        this.day = day;
+        this.hour = hour;
+    }
+
+    //Get / Set
+    public int getCourtId() {
+        return courtId;
+    }
+
+    public void setCourtId(int courtId) {
+        this.courtId = courtId;
+    }
+
+    public String getUserDNI() {
+        return userDNI;
+    }
+
+    public void setUserDNI(String userDNI) {
+        this.userDNI = userDNI;
+    }
+
+    public Calendar getDay() {
+        return day;
+    }
+
+    public void setDay(Calendar day) {
+        this.day = day;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
 }
--- src/Modelo/Court.java
+++ src/Modelo/Court.java
@@ -1,5 +1,49 @@
 package Modelo;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class Court {
+    // Attributes
+    private int ID;
+    private String name;
+    private String status;
     
+    // Constructor
+    public Court(int ID, String name, String status) {
+        this.ID = ID;
+        this.name = name;
+        this.status = status;
+    }
+    
+    // Get / Set
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    // Methods
+    public void changeStatus(String status) {
+        // llamar a la BD y update status = String status where court id = int id
+        
+    }
 }
