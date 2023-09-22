adc5df2ba80239e1a0f903f7e24c41cb4260afe3
efuentes21 <edgar.fuentes@inslapineda.cat>
2023-09-22 08:36:46 +0200

    MVC

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-        /*Hola*/
-        System.out.println("Adios mundo");
-        System.out.println("Esto es otra prueba");
-    }
-    
-}
