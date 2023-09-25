897ee9947a281fb6ec7455ecc8c287f38f565b0c
efuentes21 <edgar.fuentes@inslapineda.cat>
2023-09-25 10:33:32 +0200

    login

--- src/Controlador/Controller.java
+++ src/Controlador/Controller.java
@@ -2,19 +2,21 @@ package Controlador;
 
 import Modelo.*;
 import Vista.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 
 public class Controller {
     public static LoginMenu loginMenu = new LoginMenu();
-    
+    private static Statement statement = connectToDatabase();
+
     public static void startApp(){
         loginMenu.setTitle("Log in");
         loginMenu.setVisible(true);
-        Statement statement = connectToDatabase();
     }
     public static Statement connectToDatabase(){
         Connection connection;
-        Statement statement = null;
+        Statement estado = null;
         String driver = "com.mysql.cj.jdbc.Driver";
         String connectionUrl = "jdbc:mysql://localhost/padel";
         String user = "root";
@@ -24,11 +26,46 @@ public class Controller {
             Class.forName(driver);
             connection = DriverManager.getConnection(connectionUrl, user, passw);
             if(connection != null){
-                statement = connection.createStatement();
+                estado = connection.createStatement();
             }
         } catch (SQLException | ClassNotFoundException e ) {
             System.out.println("Error " + e.getMessage());
         }
-        return statement;
+        return estado;
+    }
+    public static void checkUser(String username, char[] password){
+        String stringPassw = String.valueOf(password);
+        System.out.println(password);
+        String consultaSQL = "SELECT * FROM user WHERE name = ? AND password = ?";
+        try {
+            // Obtener una instancia del objeto MessageDigest con el algoritmo MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // Calcular el hash de la cadena de texto
+            md.update(stringPassw.getBytes());
+            byte[] digest = md.digest();
+
+            // Convertir el hash en una representación hexadecimal
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : digest) {
+                hexString.append(String.format("%02x", b));
+            }
+
+            try(PreparedStatement prepareQuery = statement.getConnection().prepareStatement(consultaSQL)){
+                prepareQuery.setString(1, username);
+                prepareQuery.setString(2, stringPassw);
+                ResultSet queryResult = prepareQuery.executeQuery();
+                while(queryResult.next()){
+                    System.out.println("hola");
+                    System.out.println(queryResult.getInt("isActive"));
+                    System.out.println(queryResult.getInt("isAdmin"));
+                }
+            } catch(SQLException ex){
+                ex.printStackTrace();
+            }
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/Vista/LoginMenu.java
+++ src/Vista/LoginMenu.java
@@ -1,5 +1,6 @@
 package Vista;
 
+import Controlador.*;
 import java.awt.Color;
 
 public class LoginMenu extends javax.swing.JFrame {
@@ -13,23 +14,92 @@ public class LoginMenu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        tUsernamePanel = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        tPasswordPanel = new javax.swing.JPasswordField();
+        bLoginButton = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setOpaque(false);
+
+        jLabel1.setText("Username");
+
+        jLabel2.setText("Password");
+
+        bLoginButton.setText("Login");
+        bLoginButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bLoginButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(76, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(tPasswordPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 164, Short.MAX_VALUE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(tUsernamePanel))
+                .addGap(80, 80, 80))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(117, 117, 117)
+                .addComponent(bLoginButton)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(48, 48, 48)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(tUsernamePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(tPasswordPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(bLoginButton)
+                .addContainerGap(53, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 854, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(264, 264, 264)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(270, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 480, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(110, 110, 110)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(110, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void bLoginButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bLoginButtonActionPerformed
+        Controller.checkUser(tUsernamePanel.getText(), tPasswordPanel.getPassword());
+    }//GEN-LAST:event_bLoginButtonActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton bLoginButton;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPasswordField tPasswordPanel;
+    private javax.swing.JTextField tUsernamePanel;
     // End of variables declaration//GEN-END:variables
 }
