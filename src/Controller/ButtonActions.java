3290f77a4caecd06d90c25e55345b5b867e322da
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2023-09-29 14:13:03 +0200

    daychanges

---
+++ src/Controller/ButtonActions.java
@@ -0,0 +1,11 @@
+package Controller;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import View.*;
+
+public class ButtonActions {
+    public static void loadEvens(){
+    }
+}
