decb3d52996c0596323a1eec5f002d5ec191e0c3
DenisDuque <d.duque.dev@gmail.com>
2023-09-29 16:39:56 +0200

    Añadido contenido multimedia, corregidos errores en la vista

--- src/View/LoginMenu.java
+++ src/View/LoginMenu.java
@@ -1,7 +1,6 @@
 package View;
 
 import Controller.Controller;
-import Controller.PanelRound;
 import java.awt.Color;
 
 public class LoginMenu extends javax.swing.JFrame {
@@ -15,113 +14,101 @@ public class LoginMenu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        panelRound1 = new PanelRound();
-        jLabel1 = new javax.swing.JLabel();
-        tUsernamePanel = new javax.swing.JTextField();
-        jLabel2 = new javax.swing.JLabel();
-        tPasswordPanel = new javax.swing.JPasswordField();
-        bLoginButton = new javax.swing.JButton();
+        PanelBackground = new javax.swing.JPanel();
+        jLabelEmail = new javax.swing.JLabel();
+        TxtboxEmail = new javax.swing.JTextField();
+        LabelPassword = new javax.swing.JLabel();
+        TxtboxPassword = new javax.swing.JPasswordField();
+        BtnLogin = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBackground(new java.awt.Color(255, 255, 255));
+        setBackground(new java.awt.Color(0, 56, 64));
         setMinimumSize(new java.awt.Dimension(417, 582));
         setResizable(false);
         setSize(new java.awt.Dimension(417, 582));
 
-        panelRound1.setBackground(new java.awt.Color(0, 56, 64));
-        panelRound1.setForeground(new java.awt.Color(60, 63, 65, 0));
-        panelRound1.setPreferredSize(new java.awt.Dimension(412, 582));
-        panelRound1.setRoundBottomLeft(20);
-        panelRound1.setRoundBottomRight(20);
-        panelRound1.setRoundTopLeft(20);
-        panelRound1.setRoundTopRight(20);
+        PanelBackground.setBackground(new java.awt.Color(0, 56, 64));
+        PanelBackground.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
-        jLabel1.setText("Email:");
+        jLabelEmail.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
+        jLabelEmail.setForeground(new java.awt.Color(255, 255, 255));
+        jLabelEmail.setText("Email:");
+        PanelBackground.add(jLabelEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(105, 276, 50, -1));
 
-        tUsernamePanel.setPreferredSize(new java.awt.Dimension(236, 50));
-        tUsernamePanel.addActionListener(new java.awt.event.ActionListener() {
+        TxtboxEmail.setBackground(new java.awt.Color(0, 90, 91));
+        TxtboxEmail.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
+        TxtboxEmail.setForeground(new java.awt.Color(255, 255, 255));
+        TxtboxEmail.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 15, 1, 15));
+        TxtboxEmail.setPreferredSize(new java.awt.Dimension(236, 50));
+        TxtboxEmail.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                tUsernamePanelActionPerformed(evt);
+                TxtboxEmailActionPerformed(evt);
             }
         });
+        PanelBackground.add(TxtboxEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 300, -1, -1));
 
-        jLabel2.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
-        jLabel2.setText("Password:");
+        LabelPassword.setFont(new java.awt.Font("Arial", 0, 16)); // NOI18N
+        LabelPassword.setForeground(new java.awt.Color(255, 255, 255));
+        LabelPassword.setText("Password:");
+        PanelBackground.add(LabelPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(105, 369, 75, -1));
 
-        tPasswordPanel.setPreferredSize(new java.awt.Dimension(236, 50));
-
-        bLoginButton.setText("Login");
-        bLoginButton.addActionListener(new java.awt.event.ActionListener() {
+        TxtboxPassword.setBackground(new java.awt.Color(0, 90, 91));
+        TxtboxPassword.setForeground(new java.awt.Color(255, 255, 255));
+        TxtboxPassword.setPreferredSize(new java.awt.Dimension(236, 50));
+        TxtboxPassword.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                bLoginButtonActionPerformed(evt);
+                TxtboxPasswordActionPerformed(evt);
             }
         });
+        PanelBackground.add(TxtboxPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 393, 236, -1));
 
-        javax.swing.GroupLayout panelRound1Layout = new javax.swing.GroupLayout(panelRound1);
-        panelRound1.setLayout(panelRound1Layout);
-        panelRound1Layout.setHorizontalGroup(
-            panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelRound1Layout.createSequentialGroup()
-                .addContainerGap(95, Short.MAX_VALUE)
-                .addGroup(panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(tPasswordPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(tUsernamePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(80, 80, 80))
-            .addGroup(panelRound1Layout.createSequentialGroup()
-                .addGap(160, 160, 160)
-                .addComponent(bLoginButton)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        panelRound1Layout.setVerticalGroup(
-            panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelRound1Layout.createSequentialGroup()
-                .addGap(48, 48, 48)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(tUsernamePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(tPasswordPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(26, 26, 26)
-                .addComponent(bLoginButton)
-                .addContainerGap(317, Short.MAX_VALUE))
-        );
+        BtnLogin.setBackground(new java.awt.Color(0, 115, 105));
+        BtnLogin.setFont(new java.awt.Font("Segoe UI", 1, 20)); // NOI18N
+        BtnLogin.setForeground(new java.awt.Color(255, 255, 255));
+        BtnLogin.setText("Login");
+        BtnLogin.setMaximumSize(new java.awt.Dimension(236, 50));
+        BtnLogin.setMinimumSize(new java.awt.Dimension(236, 50));
+        BtnLogin.setPreferredSize(new java.awt.Dimension(236, 50));
+        BtnLogin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnLoginActionPerformed(evt);
+            }
+        });
+        PanelBackground.add(BtnLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 486, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(panelRound1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(PanelBackground, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelRound1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(PanelBackground, javax.swing.GroupLayout.DEFAULT_SIZE, 582, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void bLoginButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bLoginButtonActionPerformed
-        Controller.checkUser(tUsernamePanel.getText(), tPasswordPanel.getPassword());
-    }//GEN-LAST:event_bLoginButtonActionPerformed
+    private void BtnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnLoginActionPerformed
+        Controller.checkUser(TxtboxEmail.getText(), TxtboxPassword.getPassword());
+    }//GEN-LAST:event_BtnLoginActionPerformed
 
-    private void tUsernamePanelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tUsernamePanelActionPerformed
+    private void TxtboxEmailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TxtboxEmailActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_tUsernamePanelActionPerformed
+    }//GEN-LAST:event_TxtboxEmailActionPerformed
+
+    private void TxtboxPasswordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TxtboxPasswordActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TxtboxPasswordActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton bLoginButton;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private PanelRound panelRound1;
-    private javax.swing.JPasswordField tPasswordPanel;
-    private javax.swing.JTextField tUsernamePanel;
+    public javax.swing.JButton BtnLogin;
+    public javax.swing.JLabel LabelPassword;
+    public javax.swing.JPanel PanelBackground;
+    public javax.swing.JTextField TxtboxEmail;
+    public javax.swing.JPasswordField TxtboxPassword;
+    public javax.swing.JLabel jLabelEmail;
     // End of variables declaration//GEN-END:variables
 }
