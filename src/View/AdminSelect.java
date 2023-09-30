6eaca53336f1b19eb1fe6b2f222273bcec9f20e4
DenisDuque <d.duque.dev@gmail.com>
2023-09-30 10:54:52 +0200

    Acabado estilo de AdminSelect

--- src/View/AdminSelect.java
+++ src/View/AdminSelect.java
@@ -32,16 +32,23 @@ public class AdminSelect extends javax.swing.JFrame {
         LabelTitle1 = new javax.swing.JLabel();
         BtnLogout = new javax.swing.JButton();
         BtnClose = new javax.swing.JButton();
+        LabelAdminPanel = new javax.swing.JLabel();
+        PanelContent = new javax.swing.JPanel();
+        LabelCourts = new javax.swing.JLabel();
+        LabelCourtsImage = new javax.swing.JLabel();
+        BtnCourts = new javax.swing.JButton();
+        LabelUsers = new javax.swing.JLabel();
+        LabelUsersImage = new javax.swing.JLabel();
+        BtnUsers = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setMaximumSize(new java.awt.Dimension(1192, 582));
         setMinimumSize(new java.awt.Dimension(1192, 582));
         setUndecorated(true);
 
         PanelBackground.setBackground(new java.awt.Color(0, 56, 64));
-        PanelBackground.setMaximumSize(new java.awt.Dimension(1192, 100));
-        PanelBackground.setMinimumSize(new java.awt.Dimension(1192, 100));
-        PanelBackground.setPreferredSize(new java.awt.Dimension(1192, 100));
+        PanelBackground.setMaximumSize(new java.awt.Dimension(1192, 582));
+        PanelBackground.setMinimumSize(new java.awt.Dimension(1192, 582));
+        PanelBackground.setPreferredSize(new java.awt.Dimension(1192, 582));
         PanelBackground.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         LabelLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/headerLogo.png"))); // NOI18N
@@ -75,6 +82,49 @@ public class AdminSelect extends javax.swing.JFrame {
         BtnClose.setBorder(null);
         PanelBackground.add(BtnClose, new org.netbeans.lib.awtextra.AbsoluteConstraints(1075, 36, -1, -1));
 
+        LabelAdminPanel.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
+        LabelAdminPanel.setForeground(new java.awt.Color(255, 255, 255));
+        LabelAdminPanel.setText("Administrator Panel");
+        PanelBackground.add(LabelAdminPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 101, -1, -1));
+
+        PanelContent.setBackground(new java.awt.Color(0, 90, 91));
+        PanelContent.setMaximumSize(new java.awt.Dimension(934, 362));
+        PanelContent.setMinimumSize(new java.awt.Dimension(934, 362));
+        PanelContent.setPreferredSize(new java.awt.Dimension(934, 362));
+        PanelContent.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        LabelCourts.setFont(new java.awt.Font("Arial", 1, 28)); // NOI18N
+        LabelCourts.setForeground(new java.awt.Color(255, 255, 255));
+        LabelCourts.setText("Courts");
+        PanelContent.add(LabelCourts, new org.netbeans.lib.awtextra.AbsoluteConstraints(184, 70, -1, -1));
+
+        LabelCourtsImage.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/courts.png"))); // NOI18N
+        PanelContent.add(LabelCourtsImage, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 150, -1, -1));
+
+        BtnCourts.setBackground(new java.awt.Color(0, 115, 105));
+        BtnCourts.setBorder(null);
+        BtnCourts.setMaximumSize(new java.awt.Dimension(350, 311));
+        BtnCourts.setMinimumSize(new java.awt.Dimension(350, 311));
+        BtnCourts.setPreferredSize(new java.awt.Dimension(350, 311));
+        PanelContent.add(BtnCourts, new org.netbeans.lib.awtextra.AbsoluteConstraints(59, 25, -1, -1));
+
+        LabelUsers.setFont(new java.awt.Font("Arial", 1, 28)); // NOI18N
+        LabelUsers.setForeground(new java.awt.Color(255, 255, 255));
+        LabelUsers.setText("Users");
+        PanelContent.add(LabelUsers, new org.netbeans.lib.awtextra.AbsoluteConstraints(659, 70, -1, -1));
+
+        LabelUsersImage.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/users.png"))); // NOI18N
+        PanelContent.add(LabelUsersImage, new org.netbeans.lib.awtextra.AbsoluteConstraints(636, 150, -1, -1));
+
+        BtnUsers.setBackground(new java.awt.Color(0, 115, 105));
+        BtnUsers.setBorder(null);
+        BtnUsers.setMaximumSize(new java.awt.Dimension(350, 311));
+        BtnUsers.setMinimumSize(new java.awt.Dimension(350, 311));
+        BtnUsers.setPreferredSize(new java.awt.Dimension(350, 311));
+        PanelContent.add(BtnUsers, new org.netbeans.lib.awtextra.AbsoluteConstraints(525, 25, -1, -1));
+
+        PanelBackground.add(PanelContent, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 140, -1, -1));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -83,9 +133,7 @@ public class AdminSelect extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(PanelBackground, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(482, Short.MAX_VALUE))
+            .addComponent(PanelBackground, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -101,10 +149,18 @@ public class AdminSelect extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BtnClose;
+    private javax.swing.JButton BtnCourts;
     private javax.swing.JButton BtnLogout;
+    private javax.swing.JButton BtnUsers;
+    private javax.swing.JLabel LabelAdminPanel;
+    private javax.swing.JLabel LabelCourts;
+    private javax.swing.JLabel LabelCourtsImage;
     private javax.swing.JLabel LabelLogo;
     private javax.swing.JLabel LabelTitle;
     private javax.swing.JLabel LabelTitle1;
+    private javax.swing.JLabel LabelUsers;
+    private javax.swing.JLabel LabelUsersImage;
     private javax.swing.JPanel PanelBackground;
+    private javax.swing.JPanel PanelContent;
     // End of variables declaration//GEN-END:variables
 }
