4e998a320dcca3bc2c32ac7f7b83f090cd9f8720
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2023-09-30 18:30:39 +0200

    Ahora el header y el contenido estan separados + el header se puede mover

--- src/View/AdminSelect.java
+++ src/View/AdminSelect.java
@@ -14,7 +14,7 @@ public class AdminSelect extends javax.swing.JFrame {
     /**
      * Creates new form AdminSelect
      */
-    
+    private int posX, posY;
     
     public AdminSelect() {
         initComponents();
@@ -34,10 +34,7 @@ public class AdminSelect extends javax.swing.JFrame {
     private void initComponents() {
 
         PanelBackground = new javax.swing.JPanel();
-        LabelLogo = new javax.swing.JLabel();
-        LabelCompleteName = new javax.swing.JLabel();
-        LabelTitle = new javax.swing.JLabel();
-        BtnClose = new javax.swing.JButton();
+        Content = new javax.swing.JPanel();
         BtnReturn = new javax.swing.JButton();
         LabelAdminPanel = new javax.swing.JLabel();
         PanelContent = new javax.swing.JPanel();
@@ -47,12 +44,19 @@ public class AdminSelect extends javax.swing.JFrame {
         LabelUsers = new javax.swing.JLabel();
         LabelUsersImage = new javax.swing.JLabel();
         BtnUsers = new javax.swing.JButton();
-        BtnLogout = new javax.swing.JButton();
         BtnAdd = new javax.swing.JButton();
+        Header = new javax.swing.JPanel();
+        LabelLogo = new javax.swing.JLabel();
+        LabelCompleteName = new javax.swing.JLabel();
+        LabelTitle = new javax.swing.JLabel();
+        BtnClose = new javax.swing.JButton();
+        BtnLogout = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(1192, 582));
         setMinimumSize(new java.awt.Dimension(1192, 582));
         setUndecorated(true);
+        setPreferredSize(new java.awt.Dimension(1192, 582));
 
         PanelBackground.setBackground(new java.awt.Color(0, 56, 64));
         PanelBackground.setMaximumSize(new java.awt.Dimension(1192, 582));
@@ -60,41 +64,21 @@ public class AdminSelect extends javax.swing.JFrame {
         PanelBackground.setPreferredSize(new java.awt.Dimension(1192, 582));
         PanelBackground.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        LabelLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/headerLogo.png"))); // NOI18N
-        PanelBackground.add(LabelLogo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
-
-        LabelCompleteName.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
-        LabelCompleteName.setForeground(new java.awt.Color(255, 255, 255));
-        LabelCompleteName.setText("Complete Name");
-        LabelCompleteName.setMaximumSize(new java.awt.Dimension(280, 28));
-        LabelCompleteName.setMinimumSize(new java.awt.Dimension(280, 28));
-        LabelCompleteName.setPreferredSize(new java.awt.Dimension(280, 28));
-        PanelBackground.add(LabelCompleteName, new org.netbeans.lib.awtextra.AbsoluteConstraints(772, 36, -1, -1));
-
-        LabelTitle.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
-        LabelTitle.setForeground(new java.awt.Color(255, 255, 255));
-        LabelTitle.setText("Booking of Padel Courts");
-        PanelBackground.add(LabelTitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(89, 36, -1, -1));
-
-        BtnClose.setBackground(new java.awt.Color(0, 56, 64));
-        BtnClose.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/close.png"))); // NOI18N
-        BtnClose.setBorder(null);
-        BtnClose.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                BtnCloseActionPerformed(evt);
-            }
-        });
-        PanelBackground.add(BtnClose, new org.netbeans.lib.awtextra.AbsoluteConstraints(1130, 36, -1, -1));
+        Content.setBackground(new java.awt.Color(0, 56, 64));
+        Content.setMaximumSize(new java.awt.Dimension(1198, 482));
+        Content.setMinimumSize(new java.awt.Dimension(1198, 482));
+        Content.setPreferredSize(new java.awt.Dimension(1198, 482));
+        Content.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         BtnReturn.setBackground(new java.awt.Color(0, 56, 64));
         BtnReturn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/return.png"))); // NOI18N
         BtnReturn.setBorder(null);
-        PanelBackground.add(BtnReturn, new org.netbeans.lib.awtextra.AbsoluteConstraints(1032, 100, -1, -1));
+        Content.add(BtnReturn, new org.netbeans.lib.awtextra.AbsoluteConstraints(1040, 30, -1, -1));
 
         LabelAdminPanel.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
         LabelAdminPanel.setForeground(new java.awt.Color(255, 255, 255));
         LabelAdminPanel.setText("Administrator Panel");
-        PanelBackground.add(LabelAdminPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 101, -1, -1));
+        Content.add(LabelAdminPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 30, -1, -1));
 
         PanelContent.setBackground(new java.awt.Color(0, 90, 91));
         PanelContent.setMaximumSize(new java.awt.Dimension(934, 362));
@@ -133,17 +117,95 @@ public class AdminSelect extends javax.swing.JFrame {
         BtnUsers.setPreferredSize(new java.awt.Dimension(350, 311));
         PanelContent.add(BtnUsers, new org.netbeans.lib.awtextra.AbsoluteConstraints(525, 25, -1, -1));
 
-        PanelBackground.add(PanelContent, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 140, -1, -1));
-
-        BtnLogout.setBackground(new java.awt.Color(0, 56, 64));
-        BtnLogout.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/logout.png"))); // NOI18N
-        BtnLogout.setBorder(null);
-        PanelBackground.add(BtnLogout, new org.netbeans.lib.awtextra.AbsoluteConstraints(1075, 36, -1, -1));
+        Content.add(PanelContent, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 70, -1, -1));
 
         BtnAdd.setBackground(new java.awt.Color(0, 56, 64));
         BtnAdd.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/add.png"))); // NOI18N
         BtnAdd.setBorder(null);
-        PanelBackground.add(BtnAdd, new org.netbeans.lib.awtextra.AbsoluteConstraints(990, 100, -1, -1));
+        Content.add(BtnAdd, new org.netbeans.lib.awtextra.AbsoluteConstraints(1000, 30, -1, -1));
+
+        PanelBackground.add(Content, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 100, -1, -1));
+
+        Header.setBackground(new java.awt.Color(0, 56, 64));
+        Header.setMaximumSize(new java.awt.Dimension(1192, 100));
+        Header.setMinimumSize(new java.awt.Dimension(1192, 100));
+        Header.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                HeaderMouseDragged(evt);
+            }
+        });
+        Header.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                HeaderMousePressed(evt);
+            }
+        });
+
+        LabelLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/headerLogo.png"))); // NOI18N
+
+        LabelCompleteName.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
+        LabelCompleteName.setForeground(new java.awt.Color(255, 255, 255));
+        LabelCompleteName.setText("Complete Name");
+        LabelCompleteName.setMaximumSize(new java.awt.Dimension(280, 28));
+        LabelCompleteName.setMinimumSize(new java.awt.Dimension(280, 28));
+        LabelCompleteName.setPreferredSize(new java.awt.Dimension(280, 28));
+
+        LabelTitle.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        LabelTitle.setForeground(new java.awt.Color(255, 255, 255));
+        LabelTitle.setText("Booking of Padel Courts");
+
+        BtnClose.setBackground(new java.awt.Color(0, 56, 64));
+        BtnClose.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/close.png"))); // NOI18N
+        BtnClose.setBorder(null);
+        BtnClose.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnCloseActionPerformed(evt);
+            }
+        });
+
+        BtnLogout.setBackground(new java.awt.Color(0, 56, 64));
+        BtnLogout.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/logout.png"))); // NOI18N
+        BtnLogout.setBorder(null);
+
+        javax.swing.GroupLayout HeaderLayout = new javax.swing.GroupLayout(Header);
+        Header.setLayout(HeaderLayout);
+        HeaderLayout.setHorizontalGroup(
+            HeaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1192, Short.MAX_VALUE)
+            .addGroup(HeaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(HeaderLayout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addGroup(HeaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(HeaderLayout.createSequentialGroup()
+                            .addGap(89, 89, 89)
+                            .addComponent(LabelTitle))
+                        .addComponent(LabelLogo))
+                    .addGap(401, 401, 401)
+                    .addComponent(LabelCompleteName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(23, 23, 23)
+                    .addComponent(BtnLogout)
+                    .addGap(23, 23, 23)
+                    .addComponent(BtnClose)
+                    .addGap(0, 0, Short.MAX_VALUE)))
+        );
+        HeaderLayout.setVerticalGroup(
+            HeaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+            .addGroup(HeaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(HeaderLayout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addGroup(HeaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(LabelLogo)
+                        .addGroup(HeaderLayout.createSequentialGroup()
+                            .addGap(36, 36, 36)
+                            .addGroup(HeaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(LabelTitle)
+                                .addComponent(LabelCompleteName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(BtnLogout)
+                                .addComponent(BtnClose))))
+                    .addGap(0, 0, Short.MAX_VALUE)))
+        );
+
+        PanelBackground.add(Header, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -163,6 +225,18 @@ public class AdminSelect extends javax.swing.JFrame {
         System.exit(0);
     }//GEN-LAST:event_BtnCloseActionPerformed
 
+    private void HeaderMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_HeaderMouseDragged
+        int newX = evt.getXOnScreen() - posX;
+        int newY = evt.getYOnScreen() - posY;
+
+        setLocation(newX, newY);
+    }//GEN-LAST:event_HeaderMouseDragged
+
+    private void HeaderMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_HeaderMousePressed
+        posX = evt.getXOnScreen() - getX();
+        posY = evt.getYOnScreen() - getY();
+    }//GEN-LAST:event_HeaderMousePressed
+
     /**
      * @param args the command line arguments
      */
@@ -174,6 +248,8 @@ public class AdminSelect extends javax.swing.JFrame {
     private javax.swing.JButton BtnLogout;
     private javax.swing.JButton BtnReturn;
     private javax.swing.JButton BtnUsers;
+    private javax.swing.JPanel Content;
+    private javax.swing.JPanel Header;
     private javax.swing.JLabel LabelAdminPanel;
     private javax.swing.JLabel LabelCompleteName;
     private javax.swing.JLabel LabelCourts;
